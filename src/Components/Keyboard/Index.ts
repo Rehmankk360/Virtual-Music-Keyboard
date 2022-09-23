59bced86e1b55858ab468e3f9c758bd5ccc4d2f3
shutah64 <66064740+Shutah64@users.noreply.github.com>
2022-09-23 19:58:21 +0500

    Resolved
    closes #20

---
+++ src/Components/Keyboard/Index.ts
@@ -0,0 +1,3 @@
+export {default as Controls} from "./Controls"
+export {default as Drums} from "./Drums"
+export {default as Piano} from "./Piano"
---
+++ src/Components/Keyboard/Keyboard.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Piano, Drums, Controls } from "./Index";
+  import Speaker from "../SharedComponents/Speaker.svelte";
+</script>
+
+<div class="keyboard_wrapper">
+  <div class="keyboard">
+    <div class="top">
+      <Speaker />
+      <Controls />
+      <Drums />
+      <Speaker />
+    </div>
+    <Piano />
+  </div>
+</div>
+
+<style>
+    
+</style>
